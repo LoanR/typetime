3eee5da95f6aa8edfa6f511985c402a02689f1f3
loan robert <loan.rbrt@gmail.com>
2018-06-15 22:58:58 +0200

    Fixes #8 | SCSS architecture AND crazy linter

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import App from './App'
-import VueRouter from 'vue-router'
-import Hello from './components/HelloWorld'
-import About from './components/About'
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import Hello from './components/HelloWorld';
+import About from './components/About';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {path: '/', component: Hello},
-  {path: '/Typetime-front', component: Hello}, // gh-pages landing special case
-  {path: '/about', component: About}
-]
+    {path: '/', component: Hello},
+    {path: '/Typetime-front', component: Hello}, // gh-pages landing special case
+    {path: '/about', component: About}
+];
 
 const router = new VueRouter({
-  routes,
-  mode: 'history'
-})
+    routes,
+    mode: 'history'
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App },
-  router
-}).$mount('#app')
+    el: '#app',
+    template: '<App/>',
+    components: { App },
+    router
+}).$mount('#app');
