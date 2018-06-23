d80bd28d677204c9b9d00876f34ce360e1648f70
loan robert <loan.rbrt@gmail.com>
2018-06-23 17:49:29 +0200

    References #62 | Hand made src and href test and public path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Typetime-front/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
