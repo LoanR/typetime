e598e0846e7d9c32582fe0330f2a358256e639c6
loan robert <loan.rbrt@gmail.com>
2018-06-26 17:28:18 +0200

    Fixes #74 | Kiss and flat buttons

--- src/components/buttons/Checkbox.vue
+++ src/components/buttons/Checkbox.vue
@@ -52,16 +52,18 @@ export default {
     }
 
     input:active + .button-style, input:checked + .button-style {
+        border: 2px solid $brand-color;
         box-shadow:
-            inset 0 1px rgba($contrast-color, 0.5),
-            inset 0 2rem 30px rgba(255, 255, 255, 0.08),
-            0px 2px rgba(lighten( $brand-color, 20% ), 1),
-            0px 2px rgba(lighten( $brand-color, 20% ), 1),
-            0px 2px rgba(lighten( $brand-color, 20% ), 1),
-            0px 2px rgba(lighten( $brand-color, 20% ), 1),
-            0px 0px 5px 0px rgba(lighten( $brand-color, 60% ), 0.8),
-            0px 4px 5px 0px rgba(lighten( $brand-color, 60% ), 0.8),
-            0px 4px 30px 0px rgba(lighten( $brand-color, 60% ), 0.5);
+            0px 0px 5px 0px rgba(lighten( $brand-color, 30% ), 0.9),
+            0px 0px 30px 0px rgba(lighten( $brand-color, 20% ), 0.5),
+            0px 0px 0px 1px rgba(lighten($brand-color, 15%), 1);
+        //     inset 0 1px rgba($contrast-color, 0.5),
+        //     inset 0 2rem 30px rgba(255, 255, 255, 0.08),
+        //     0px 2px rgba(lighten( $brand-color, 20% ), 1),
+        //     0px 2px rgba(lighten( $brand-color, 20% ), 1),
+        //     0px 2px rgba(lighten( $brand-color, 20% ), 1),
+        //     0px 2px rgba(lighten( $brand-color, 20% ), 1),
+        //     0px 0px 5px 0px rgba(lighten( $brand-color, 60% ), 0.8),
         background-color: $brand-color;
 
         &::before, &::after {
@@ -70,28 +72,28 @@ export default {
 
         span {
             color: $light-base-color;
-            text-shadow:
-                0px 0px 2px rgba(lighten( $brand-color, 60% ), 1),
-                0px 1px 10px rgba(lighten( $brand-color, 60% ), 1);
+            // text-shadow:
+            //     0px 0px 2px rgba(lighten( $brand-color, 60% ), 1),
+            //     0px 1px 10px rgba(lighten( $brand-color, 60% ), 1);
         }
     }
 
-    input:active + .button-style span{
-        text-shadow:
-            0px 0px 2px rgba(lighten( $brand-color, 60% ), 1),
-            0px 1px 10px rgba(lighten( $brand-color, 60% ), 1);
-    }
+    // input:active + .button-style span{
+    //     text-shadow:
+    //         0px 0px 2px rgba(lighten( $brand-color, 60% ), 1),
+    //         0px 1px 10px rgba(lighten( $brand-color, 60% ), 1);
+    // }
 
-    input:checked:not(:active) + .button-style {
-        box-shadow:
-            inset 0 1px rgba($contrast-color, 0.5),
-            inset 0 2rem 30px rgba(255, 255, 255, 0.08),
-            0px 2px rgba(lighten( $brand-color, 20% ), 1),
-            0px 3px rgba(lighten( $brand-color, 20% ), 1),
-            0px 4px rgba(lighten( $brand-color, 20% ), 1),
-            0px 5px rgba(lighten( $brand-color, 20% ), 1),
-            0px 0px 5px 0px rgba(lighten( $brand-color, 60% ), 0.8),
-            0px 7px 5px 0px rgba(lighten( $brand-color, 60% ), 0.8),
-            0px 4px 30px 0px rgba(lighten( $brand-color, 60% ), 0.5);
-    }
+    // input:checked:not(:active) + .button-style {
+    //     box-shadow:
+    //         inset 0 1px rgba($contrast-color, 0.5),
+    //         inset 0 2rem 30px rgba(255, 255, 255, 0.08),
+    //         0px 2px rgba(lighten( $brand-color, 20% ), 1),
+    //         0px 3px rgba(lighten( $brand-color, 20% ), 1),
+    //         0px 4px rgba(lighten( $brand-color, 20% ), 1),
+    //         0px 5px rgba(lighten( $brand-color, 20% ), 1),
+    //         0px 0px 5px 0px rgba(lighten( $brand-color, 60% ), 0.8),
+    //         0px 7px 5px 0px rgba(lighten( $brand-color, 60% ), 0.8),
+    //         0px 4px 30px 0px rgba(lighten( $brand-color, 60% ), 0.5);
+    // }
 </style>
