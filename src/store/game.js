2a2f1e2250788a18359a57ff4a6ba4afed0fc073
loan robert <loan.rbrt@gmail.com>
2018-12-15 01:12:31 +0100

    Fixes #82 | Build and tweaks for prod tests

--- src/store/game.js
+++ src/store/game.js
@@ -6,16 +6,16 @@ export const gameMutations = {
         state.rules.levelRules.currentLevel += 1;
         state.rules.levelRules.wordAmount += 1;
         state.rules.levelRules.wordsPerMinute += 1;
-        // state.rules.levelRules.allotedWordBaseTime = parseInt((6000 / state.rules.levelRules.wordsPerMinute).toFixed());
-        state.rules.levelRules.allotedWordBaseTime = 600; // dev env purpose
+        state.rules.levelRules.allotedWordBaseTime = parseInt((6000 / state.rules.levelRules.wordsPerMinute).toFixed());
+        // state.rules.levelRules.allotedWordBaseTime = 600; // by hand tests purpose
     },
 
     resetLevelRules(state) {
         state.rules.levelRules.currentLevel = 1;
         state.rules.levelRules.wordAmount = BASE_WORD_AMOUNT;
         state.rules.levelRules.wordsPerMinute = BASE_WORD_PER_MINUTE;
-        // state.rules.levelRules.allotedWordBaseTime = parseInt(6000 / BASE_WORD_PER_MINUTE);
-        state.rules.levelRules.allotedWordBaseTime = 600; // dev env purpose
+        state.rules.levelRules.allotedWordBaseTime = parseInt(6000 / BASE_WORD_PER_MINUTE);
+        // state.rules.levelRules.allotedWordBaseTime = 600; // by hand tests purpose
         state.rules.levelRules.savedWordTime = 0;
     },
 
